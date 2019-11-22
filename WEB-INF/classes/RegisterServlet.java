34b0185a6fbab641c742cd4d177aec9f3ca80ac8
morenoloza98 <morenolozalex98@gmail.com>
2019-11-21 23:22:53 -0600

    redirecting to the initial quiz

--- WEB-INF/classes/RegisterServlet.java
+++ WEB-INF/classes/RegisterServlet.java
@@ -79,7 +79,7 @@ public class RegisterServlet extends HttpServlet{
 
 			//!!!!!!---------   DEBUGGING FINISHES - Creating a JSP with all registered users example !!!!!!---------
 
-			response.sendRedirect("./");
+			response.sendRedirect("./initialQuiz.jsp");
 
 		}
 		catch(Exception e){
